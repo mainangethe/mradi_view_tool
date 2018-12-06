5fda3cd994f66aa6e389f6c79fbb75d1a4f39fed
Ng'ethe Maina <ngethe@mradi.co.ke>
2018-12-06 10:11:32 +0300

    Fix HTML Renderer bug
    Found a method syntax bug '.html.safe' instead of '.html_safe'

--- lib/mradi_view_tool/renderer.rb
+++ lib/mradi_view_tool/renderer.rb
@@ -2,7 +2,7 @@ module MradiViewTool
 	class Error < StandardError; end
 	class Renderer
   		def self.copyright(name, msg)
-  			"&copy; #{Time.now.year} | <b> #{name}</b> #{msg}".html.safe
+  			"&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
   		end
   	end
 end
